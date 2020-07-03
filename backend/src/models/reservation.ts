23426264a05cdf986c1f314ab69ac926d6295378
punisher077 <arthur.mts@gmail.com>
2020-07-03 13:47:59 -0300

    refactor: 💡 add status to reservation

--- backend/src/models/reservation.ts
+++ backend/src/models/reservation.ts
@@ -1,23 +1,34 @@
 import { Schema, Types, model, Document } from 'mongoose';
 
+export enum ReservationStatus {
+  PENDING = 'PENDING',
+  REFUSED = 'REFUSED',
+  APPROVED = 'APPROVED',
+  CLOSED = 'CLOSED',
+}
+
 export interface IReservationSchema extends Document {
   userId: Types.ObjectId;
   establishmentId: Types.ObjectId;
   schedule: Date;
-  aprooved: boolean | null;
-  closed: boolean | null;
-  feedback: null | Types.ObjectId;
+  feedback?: Types.ObjectId;
+  status?: ReservationStatus;
 }
 
+// pending, refused, approved, closed
 const ReservationSchema: Schema = new Schema({
   userId: { type: Types.ObjectId, ref: 'User' },
   establishmentId: { type: Types.ObjectId, ref: 'Establishment' },
   schedule: Date,
-  aprooved: Boolean,
-  closed: Boolean,
+  status: {
+    type: String,
+    enum: ['PENDING', 'REFUSED', 'APPROVED', 'CLOSED'],
+    default: 'PENDING',
+  },
   feedback: {
     type: Types.ObjectId,
     ref: 'Feedback',
+    default: undefined,
   },
 });
 
